a69c777f9cf54ffb0205d12f595864c3d1d5f4b7
IEMDomain04.dc <emmanman0011@gmail.com>
2024-10-18 22:31:56 +0800

    code added semantic html feature

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 const app = document.getElementById('passGenerator-app')
 
 const htmlTag = `
-    <div id="container">
+    <main id="container">
 
-        <div id="header-container">
+        <section id="header-container">
             <h1>Generate a<br><span style="color: #55F991;"> random password</span></h1>
             <h2>Never use an insecure password again.</h2>
             <button id="generate-btn" onclick="generatePassword()">Generate passwords</button>
-        </div>
+        </section>
         
         <hr>
 
-        <div id="output-container">
+        <section id="output-container">
             <h3 class="passwords" id="password1" onclick="copyPassword1()"></h3>
             <h3 class="passwords" id="password2" onclick="copyPassword2()"></h3>
-        </div>
+        </section>
 
-    </div>
+    </main>
 `
 
 app.innerHTML = htmlTag
